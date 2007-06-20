1e75d237dbffbafdd7137098e012fbb1761e0ac8
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2007-06-20 12:31:10 +0000

    Modifications for pressure matching problem and minor edits in testcases

--- optim/src/opt.h
+++ optim/src/opt.h
@@ -1,4 +1,6 @@
 double cl, cd, clref, cdref, cost0;
+double CP0[1000], CP[1000];
+int costfun;
 char rundir[100];
 char deform[100];
 char flosol[100];
--- optim/src/readParam.c
+++ optim/src/readParam.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include "opt.h"
 
 void readParam(int *iopt, double *ropt)
 {
    FILE *fpt;
    char dummy[100];
+   int nsolid, i;
+   double rdummy;
 
    fpt = fopen("optim.inp", "r");
    fscanf(fpt, "%s%d%d", dummy, &iopt[0], &iopt[1]);
    fscanf(fpt, "%s%d", dummy, &iopt[2]);
+   fscanf(fpt, "%s%d", dummy, &iopt[3]);
 
    fscanf(fpt, "%s%lf", dummy, &ropt[0]);
    fscanf(fpt, "%s%lf", dummy, &ropt[1]);
@@ -15,5 +19,19 @@ void readParam(int *iopt, double *ropt)
    fscanf(fpt, "%s%lf", dummy, &ropt[3]);
    fscanf(fpt, "%s%lf", dummy, &ropt[4]);
    fclose(fpt);
+
+   /* If costfun==1, i.e., pressure matching problem, then read the target
+    * pressure distribution. Note that the order of points in this file must
+    * match the order in which the flow-solver outputs the pressure
+    * coefficient into WALL.DAT */
+   if(iopt[3] == 1){
+      printf("Reading cp0.dat ... ");
+      fpt = fopen("cp0.dat", "r");
+      fscanf(fpt,"%d", &nsolid);
+      printf("there are %5d points in there\n", nsolid);
+      for(i=0; i<nsolid; i++)
+         fscanf(fpt,"%lf%lf%lf", &rdummy, &CP0[i], &rdummy);
+      fclose(fpt);
+   }
 
 }
