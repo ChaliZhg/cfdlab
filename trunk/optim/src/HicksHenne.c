9f1b5317dffe6a7617de270aaacdcd64d641676b
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2007-06-25 13:29:09 +0000

    minor changes in objCFD.c and new function HicksHenneFun added

--- trunk/optim/src/HicksHenne.c
+++ trunk/optim/src/HicksHenne.c
@@ -3,18 +3,25 @@
 double HicksHenne(int n, double *a, double x)
 {
    int i;
-   double xh, tmp1, tmp2, tmp3, tmp4, y, hpower = 3.0;
+   double y;
+   double HicksHenneFun(int, int, double);
 
    y = 0.0;
-   for(i = 0; i < n; i++) {
+   for(i = 0; i < n; i++)
+      y += a[i] * HicksHenneFun(i, n, x);
+
+   return y;
+
+}
+
+/* i'th Hicks-Henne function */
+double HicksHenneFun(int i, int n, double x){
+   double xh, tmp1, tmp2, tmp3, tmp4, hpower = 3.0;
+
       xh = ((double) (i + 3)) / (n + 5);
       tmp1 = log(0.5) / log(xh);
       tmp2 = M_PI * pow(x, tmp1);
       tmp3 = sin(tmp2);
       tmp4 = pow(tmp3, hpower);
-      y += a[i] * tmp4;
-   }
-
-   return y;
-
+      return tmp4;
 }
