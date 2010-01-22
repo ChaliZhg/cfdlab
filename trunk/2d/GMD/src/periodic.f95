d63593112c0956d7478d93ab35c869944b83067f
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2010-01-22 13:31:04 +0000

    Added option to set periodic bc in each direction, set xperiod and yperiod in main.f95

--- trunk/2d/GMD/src/periodic.f95
+++ trunk/2d/GMD/src/periodic.f95
@@ -6,42 +6,50 @@ subroutine periodic(con)
 
    integer :: i, j
 
-   do j=1,ny
-      con(:,-1,  j) = con(:,nx-1,j)
-      con(:, 0,  j) = con(:,nx,  j)
-      con(:,nx+1,j) = con(:,1,   j)
-      con(:,nx+2,j) = con(:,2,   j)
-   enddo
+   if(xperiod == yes)then
+      do j=1,ny
+         con(:,-1,  j) = con(:,nx-1,j)
+         con(:, 0,  j) = con(:,nx,  j)
+         con(:,nx+1,j) = con(:,1,   j)
+         con(:,nx+2,j) = con(:,2,   j)
+      enddo
+   endif
 
-   do i=1,nx
-      con(:,i,  -1) = con(:,i,ny-1)
-      con(:,i,   0) = con(:,i,ny  )
-      con(:,i,ny+1) = con(:,i,1   )
-      con(:,i,ny+2) = con(:,i,2   )
-   enddo
+   if(yperiod == yes)then
+      do i=1,nx
+         con(:,i,  -1) = con(:,i,ny-1)
+         con(:,i,   0) = con(:,i,ny  )
+         con(:,i,ny+1) = con(:,i,1   )
+         con(:,i,ny+2) = con(:,i,2   )
+      enddo
+   endif
 
-   ! lower left corner
-   con(:, 0, 0) = con(:,nx,  ny)
-   con(:,-1, 0) = con(:,nx-1,ny)
-   con(:, 0,-1) = con(:,nx,  ny-1)
-   con(:,-1,-1) = con(:,nx-1,ny-1)
+   if(xperiod == yes .and. yperiod == yes)then
 
-   ! lower right corner
-   con(:,nx+1, 0) = con(:,1,ny)
-   con(:,nx+2, 0) = con(:,2,ny)
-   con(:,nx+1,-1) = con(:,1,ny-1)
-   con(:,nx+2,-1) = con(:,2,ny-1)
+      ! lower left corner
+      con(:, 0, 0) = con(:,nx,  ny)
+      con(:,-1, 0) = con(:,nx-1,ny)
+      con(:, 0,-1) = con(:,nx,  ny-1)
+      con(:,-1,-1) = con(:,nx-1,ny-1)
 
-   ! upper left corner
-   con(:, 0,ny+1) = con(:,nx,  1)
-   con(:, 0,ny+2) = con(:,nx,  2)
-   con(:,-1,ny+1) = con(:,nx-1,1)
-   con(:,-1,ny+2) = con(:,nx-1,2)
+      ! lower right corner
+      con(:,nx+1, 0) = con(:,1,ny)
+      con(:,nx+2, 0) = con(:,2,ny)
+      con(:,nx+1,-1) = con(:,1,ny-1)
+      con(:,nx+2,-1) = con(:,2,ny-1)
 
-   ! upper right corner
-   con(:,nx+1,ny+1) = con(:,1,1)
-   con(:,nx+2,ny+1) = con(:,2,1)
-   con(:,nx+1,ny+2) = con(:,1,2)
-   con(:,nx+2,ny+2) = con(:,2,2)
+      ! upper left corner
+      con(:, 0,ny+1) = con(:,nx,  1)
+      con(:, 0,ny+2) = con(:,nx,  2)
+      con(:,-1,ny+1) = con(:,nx-1,1)
+      con(:,-1,ny+2) = con(:,nx-1,2)
+
+      ! upper right corner
+      con(:,nx+1,ny+1) = con(:,1,1)
+      con(:,nx+2,ny+1) = con(:,2,1)
+      con(:,nx+1,ny+2) = con(:,1,2)
+      con(:,nx+2,ny+2) = con(:,2,2)
+
+   endif
 
 end subroutine periodic
