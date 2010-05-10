b7139c27a46eb8c381c27ff4ea0b3de7914e29fd
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2010-05-10 13:33:23 +0000

    Cleaned up limiter implementation by removing duplication

---
+++ 1d/bspline_DG/project.m
@@ -0,0 +1,26 @@
+% p = degree of bspline
+% N = number of cells = no. of knot intervals
+% cflmode = 'zhang' or 'praveen'
+
+function [Ul, ubar, mintheta] = project(U)
+
+globals;
+
+[N,p] = size(U);
+p = p - 1;
+
+mintheta = 1.0e20;
+
+% limiter
+for j=1:N
+   ubar(j) = sum(U(j,:))/(p+1);
+   ff = bezier(U(j,:), xx);
+   mj = min(ff);
+   Mj = max(ff);
+   a1 = abs(umax - ubar(j))/abs(Mj - ubar(j));
+   a2 = abs(umin - ubar(j))/abs(mj - ubar(j));
+   theta = min( [a1, a2, 1.0] );
+   Ul(j,:) = theta*(U(j,:) - ubar(j)) + ubar(j);
+   mintheta = min(mintheta, theta);
+end
+
