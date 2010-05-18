4bdc3c861126f31844e8def8c63dd44f2c8b3802
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2010-05-18 09:27:53 +0000

    Interior knots must be repeated k+1 times only

--- trunk/1d/bspline_DG/bspdisc.m
+++ trunk/1d/bspline_DG/bspdisc.m
@@ -4,14 +4,14 @@ close all
 k=2; % degree
 nk=4; % no of distinct knots
 
-m = (k+1) + (nk-2)*(k+2) + (k+1) - 1;
+m = (k+1) + (nk-2)*(k+1) + (k+1) - 1;
 
 n = m - k - 1; % n+1 = no of control points
 
 % knot vector
 knot=[zeros(1,k+1)];
 for j=2:nk-1
-   knot=[knot (j-1)*ones(1,k+2)];
+   knot=[knot (j-1)*ones(1,k+1)];
 end
 knot=[knot (nk-1)*ones(1,k+1)];
 
@@ -33,7 +33,7 @@ end
 figure(2)
 cont=zeros(1,n+1); 
 cont(1,1:3)=1.0;
-cont(1,5:7)=2.0;
-cont(1,9:11)=3.0;
+cont(1,4:6)=2.0;
+cont(1,7:9)=3.0;
 p=bspeval(k,cont,knot,u);
 plot(u,p)
