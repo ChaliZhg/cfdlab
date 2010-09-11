d4666e3fe718eb85b9850cc884b4bcc845cd7943
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2010-09-11 05:55:34 +0000

    Updated code

--- 1d/nozzle/exact/densityeq.m
+++ 1d/nozzle/exact/densityeq.m
@@ -2,5 +2,16 @@ function f = densityeq(r, x, s1)
 
 global gamma frate H0
 
+if r <= 0.0
+   disp('Density is negative')
+   disp([r x s1])
+   pause
+   f = 1.0e20;
+   return
+end
+
 a = nozarea(x);
-f = gamma*s1*r^(gamma-1)/(gamma-1) + 0.5*frate^2/(r^2*a^2) - H0;
+p = s1*r^gamma;
+c = sqrt(gamma*p/r);
+u = frate/(r*a);
+f = c^2/(gamma-1) + 0.5*u^2 - H0;
