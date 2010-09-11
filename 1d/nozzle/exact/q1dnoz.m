12faa88a32a08f1275d3b962c045ff76f0ae9dbc
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2010-09-11 06:30:25 +0000

    Fixed some conflicts in q1dnoz, there could be still bugs. Check before using

--- 1d/nozzle/exact/q1dnoz.m
+++ 1d/nozzle/exact/q1dnoz.m
@@ -1,7 +1,9 @@
+% THIS PROGRAM COULD HAVE BUGS
+% Check as and as1, they could be same
 clear all
 close all
 
-global gamma H0 frate L xthroat athroat as1 p0 noztyp
+global gamma H0 frate L xthroat athroat as as1 p0 noztyp
 global xs s1 r1
 
 gamma=1.4;
@@ -13,14 +15,20 @@ x1 = 0.0; % inlet
 a1 = nozarea(x1); % inlet area
 x2 = L; % L is set when nozarea is called above
 
+as = athroat;
+
 % Inflow conditions
-m1=1.5;
+m1=machnum(0); % mach number
+%m1=1.5;
 ff = (1 + 0.5*(gamma-1)*m1^2)*2/(gamma+1);
 ff = (ff)^((gamma+1)/(gamma-1));
 ff = ff/m1/m1;
 as1= a1/sqrt(ff)
+
 r1=1.0; % density
 p1=1.0;
+
+
 p0=p1*(1 + 0.5*(gamma-1)*m1^2)^(gamma/(gamma-1));
 
 % pressure ratio: Poutlet/Pinlet
@@ -36,18 +44,45 @@ H0 = c1^2/(gamma-1.0) + 0.5*u1^2;
 % outflow pressure
 p2=p1*prat;
 
+% Compute area at sonic conditions
+%ff = (1 + 0.5*(gamma-1)*m1^2)*2/(gamma+1);
+%ff = (ff)^((gamma+1)/(gamma-1));
+%ff = ff/m1/m1;
+%as = a1/sqrt(ff)
+%as = 1
+
 % flow rate: this is constant
 frate = r1*u1*a1;
 
+machnum(0)
+machnum(1)
+machnum(2)
+machnum(3)
+machnum(4)
+machnum(5)
+pause
+
+%x = linspace(0,4.5,30);
+%for j=1:30
+%r(j) = density(x(j), s1, r1);
+%end
+%
+%load flow.dat;
+%plot(x,r,'o',flow(:,1),flow(:,2))
+
 % Find shock location xs
 fun = @(x) shockfun(x, x2, r1, s1, p2);
-xs = fminbnd(fun, x1, x2, optimset('TolX',1e-12))
+xs = fminbnd(fun, 5, x2, optimset('TolX',1e-12))
 fun(xs)
+
+pause
 
 % integral of pressure
 fun = @(x) pressure(xs, r1, s1, x);
 integral = quadgk(fun, x1, x2, 'Waypoints', [xs], 'AbsTol', 1e-10, 'RelTol', 0);
 fprintf(1,'Pressure Integral = %24.15e\n', integral);
+
+pause
 
 % Load numerical solution
 %load flow.dat
