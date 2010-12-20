3d29a59347c0e35a4f5915dcb641f41196be0c8e
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2010-12-20 06:11:29 +0000

    Finished stochastic refinement in 1d

--- trunk/uq/uqtool/src/refine_grid_1d.cc
+++ trunk/uq/uqtool/src/refine_grid_1d.cc
@@ -10,12 +10,44 @@
 #include "uq.h"
 
 // Refine the stochastic grid in 1-D
+// TBD We are assuming second order elements
+// old element:  0-----2-----1
+// new element:  0--*--2--*--1
 template <>
 void UQProblem<1>::refine_grid ()
 {
    for(unsigned int i=0; i<grid.element.size(); ++i)
       if(grid.element[i].active && grid.element[i].refine_flag)
       {
-         // TBD refine this element into two
+         // Refine this element into two
+         
+         // Two new samples
+         Sample<1> new_sample1 (n_var, n_cell, n_moment, sample.size());
+         new_sample1.x[0] = (grid.element[i].dof[0]->x[0] + 
+                             grid.element[i].dof[2]->x[0]) / 2.0;
+         sample.push_back (new_sample1);
+
+         Sample<1> new_sample2 (n_var, n_cell, n_moment, sample.size());
+         new_sample2.x[0] = (grid.element[i].dof[1]->x[0] + 
+                             grid.element[i].dof[2]->x[0]) / 2.0;
+         sample.push_back (new_sample2);
+         
+         // Two new elements
+         Element<1> new_element1 (2, n_moment);
+         new_element1.parent = &grid.element[i];
+         new_element1.dof[0] = grid.element[i].dof[0];
+         new_element1.dof[1] = grid.element[i].dof[2];
+         new_element1.dof[2] = &sample[sample.size()-2];
+         grid.element.push_back (new_element1);
+
+         Element<1> new_element2 (2, n_moment);
+         new_element2.parent = &grid.element[i];
+         new_element2.dof[0] = grid.element[i].dof[2];
+         new_element2.dof[1] = grid.element[i].dof[1];
+         new_element2.dof[2] = &sample[sample.size()-1];
+         grid.element.push_back (new_element2);
+         
+         grid.element[i].active = false;
+         grid.element[i].refine_flag = false;
       }
 }
