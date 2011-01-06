d0dc20cb492276663c224707d74a9e6fb9cc6b21
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2011-01-06 16:56:06 +0000

    uqtool: Added cell_map. Allows to do physical adaptation in future.

--- trunk/uq/uqtool/examples/conv_diff/src/ccre.f
+++ trunk/uq/uqtool/examples/conv_diff/src/ccre.f
@@ -1,17 +1,27 @@
       program main
       implicit none
-      integer :: nc, i
+      integer :: nc, nc_h, i, j
       real,allocatable :: v(:), R(:), AR(:), dq(:), dv(:)
+      real,allocatable :: cc(:), re(:)
+      integer,allocatable :: cell_map(:)
 
-      open(10,file='grid.dat',status='old')
-      read(10,*) nc
+      write(*,*) 'Reading cell_map.dat'
+      open(10,file='cell_map.dat',status='old')
+      read(10,*) nc, nc_h
+      allocate(cell_map(nc_h) )
+      do i=1,nc_h
+         read(10,*) cell_map(i)
+      enddo
       close(10)
 
-      allocate( v(nc) )
-      allocate( R(nc) )
-      allocate(AR(nc) )
-      allocate(dq(nc) )
-      allocate(dv(nc) )
+      allocate( v(nc_h) )
+      allocate( R(nc_h) )
+      allocate(AR(nc_h) )
+      allocate(dq(nc_h) )
+      allocate(dv(nc_h) )
+
+      allocate(cc(nc))
+      allocate(re(nc))
 
 c     Read adjoint
 c     Read primal residual
@@ -23,7 +33,7 @@ c     Read dadjoint
       open(12,file='a_residual.dat',status='old')
       open(13,file='dprimal.dat',   status='old')
       open(14,file='dadjoint.dat',  status='old')
-      do i=1,nc
+      do i=1,nc_h
          read(10,*) v(i)
          read(11,*) R(i)
          read(12,*) AR(i)
@@ -37,11 +47,19 @@ c     Read dadjoint
       close(13)
       close(14)
 
+      cc = 0.0
+      re = 0.0
+      do i=1,nc_h
+         j = cell_map(i)
+         cc(j) = cc(j) + v(i)*R(i)
+         re(j) = re(j) + dv(i)*R(i) + dq(i)*AR(i)
+      enddo
+
       open(10,file='VdotR.dat')
       open(11,file='RE.dat')
       do i=1,nc
-         write(10,'(e24.14)') v(i)*R(i)
-         write(11,'(e24.14)') dv(i)*R(i) + dq(i)*AR(i)
+         write(10,'(e24.14)') cc(i)
+         write(11,'(e24.14)') re(i)
       enddo
       close(10)
       close(11)
