3df8ac630d8505bc27324a3f5873bb1b4c8e2be1
karthik.du@gmail.com <karthik.du@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2011-01-14 19:07:53 +0000

    New

--- uq/uqtool/examples/conv_diff/src/calc_grad.f
+++ uq/uqtool/examples/conv_diff/src/calc_grad.f
@@ -20,8 +20,8 @@
 
       subroutine calc_grad_lsq(qm,q,qp,xm,x,xp,grads)
       implicit none
-      real :: qm,q,qp,xm,x,xp,grads
-      real :: w1,w2,epsil,du1,du2,dx1,dx2
+      real :: qm,q,qp,xm,x,xp,grads,qxp,qxm
+      real :: w1,w2,epsil,du1,du2,dx1,dx2,xvm,xvp,qmax,qmin,phi
 
       epsil = 1.e-12
 
@@ -30,10 +30,32 @@
       dx1=x-xm
       dx2=xp-x
 
-      w1=1.d0
-      w2=1.d0
+       w1=1.d0
+       w2=1.d0
 
       grads = (w1*dx1*du1+w2*dx2*du2)/(w1*dx1*dx1+w2*dx2*dx2)
+
+      xvm = 0.5*(x+xm)
+      xvp = 0.5*(x+xp)
+
+      qmax=max(max(q,qm),qp)
+      qmin=min(min(q,qm),qp)
+
+      qxp = q+(xvp-x)*grads
+      qxm = q+(xvm-x)*grads
+
+      phi = 1.d0
+
+      if((qxp-q).gt.0.) phi = min(phi,(qmax-q)/(qxp-q))
+
+      if((qxp-q).lt.0.) phi = min(phi,(qmin-q)/(qxp-q))
+
+      if((qxm-q).gt.0.) phi = min(phi,(qmax-q)/(qxm-q))
+
+      if((qxm-q).lt.0.) phi = min(phi,(qmin-q)/(qxm-q))
+
+
+       !grads = grads*phi
 
       return
       end
