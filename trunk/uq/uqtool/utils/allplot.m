17306375239b560a1137c9319c0c1af679892d1e
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2011-01-16 08:50:18 +0000

    uqtool: added convergence rate calculation in allplot.m

--- trunk/uq/uqtool/utils/allplot.m
+++ trunk/uq/uqtool/utils/allplot.m
@@ -1,6 +1,10 @@
 % Program to find and plot convergence rate of CC and RE
 % Ignore first iskip data points
-function allplot ()
+function allplot (istart)
+
+if nargin==0
+   istart=1;
+end
 
 Jexact = load('Jexact.dat')
 
@@ -20,6 +24,7 @@ Jc_error= abs(Jc - Jexact);
 figure(1)
 plot(Ns,J,'o-',Ns,Jc,'o--','LineWidth',1.5)
 xlabel('Number of samples')
+ylabel('Mean functional')
 legend('J','J+CC')
 
 figure(2)
@@ -27,4 +32,21 @@ loglog(h,J_error,'o--',...
        h,Jc_error,'*--',...
        h,cc,...
        h,re,'LineWidth',1.5);
+xlabel('\Delta\xi')
 legend('Error in J','Error in J+CC','CC','RE')
+
+% Convergence rates
+ns = length(h);
+x = h(istart:ns);
+
+% for J
+f = J_error(istart:ns);
+A = [ones(size(x)), log10(x)];
+sol = inv(A'*A)*A'*log10(f);
+rate = sol(2)
+
+% for J+CC
+f = Jc_error(istart:ns);
+A = [ones(size(x)), log10(x)];
+sol = inv(A'*A)*A'*log10(f);
+rate = sol(2)
