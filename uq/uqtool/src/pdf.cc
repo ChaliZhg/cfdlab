27636342a1c8e03d391d4712dafd2a615bd0d730
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2011-06-14 04:46:45 +0000

    uqtool: added beta distribution

--- uq/uqtool/src/pdf.cc
+++ uq/uqtool/src/pdf.cc
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <cmath>
 #include "pdf.h"
+#include "prob.h"
 
 using namespace std;
 
@@ -18,14 +19,14 @@ using namespace std;
 template <int dim>
 double PDFData<dim>::get_pdf (const double* x) const
 {
-   double pdf = 1.0, p;
+   double pdf = 1.0, p, xx;
    
    for(unsigned int i=0; i<dim; ++i)
    {
       switch (type[i])
       {
          // Uniform random variable
-         case (int)uniform:
+         case (int)PDFType::uniform:
             if(x[i]>=x_min[i] && x[i]<=x_max[i])
                p = 1.0/(x_max[i] - x_min[i]);
             else
@@ -33,10 +34,23 @@ double PDFData<dim>::get_pdf (const double* x) const
             break;
             
          // Normal/gaussian random variable
-         case (int)normal:
+         case (int)PDFType::normal:
             p = exp(-0.5 * (x[i] - mean[i]) * (x[i] - mean[i]) / variance[i]);
             p = p / sqrt(2.0 * M_PI * variance[i]);
             break;
+
+         // Beta random variable
+         case (int)PDFType::beta:
+            xx = (x[i] - x_min[i])/(x_max[i] - x_min[i]);
+            // Prevent xx going beyond [0,1] due to round-off
+            xx = max(0.0, xx);
+            xx = min(1.0, xx);
+            p = Probability::beta_pdf (xx, alpha[i], beta[i]);
+            break;
+
+         case (int)PDFType::lognormal:
+            cout << "lognormal not implemented\n";
+            abort ();
 
          default:
             cout << "Unknown PDF !!!" << endl;
