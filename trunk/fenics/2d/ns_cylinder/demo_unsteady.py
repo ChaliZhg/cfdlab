559cb6bbc3a6bd8a180e310cdc66a3c787266ba2
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2011-10-01 10:16:32 +0000

    fenics: unsteady flow for cylinder in channel

---
+++ trunk/fenics/2d/ns_cylinder/demo_unsteady.py
@@ -0,0 +1,106 @@
+"""
+This demo program solves the unsteady incompressible Navier-Stokes equations
+for cylinder in channel problem using Taylor-Hood elements.
+   Author: Praveen. C
+   www   : http://math.tifrbng.res.in/~praveen
+"""
+
+# Set parameter values
+Re   = 200
+D    = 0.1
+Uinf = 1.0
+nu   = D * Uinf / Re
+
+theta= 0.5  # implicit scheme
+cfl  = 1.0  # cfl number
+tf   = 1e-1 # final time
+
+# Functions to perturb the boundary velocity
+def g(t):
+   if t <= -1.0:
+      return 0.0
+   elif t >= 1.0:
+      return 1.0
+   else:
+      return 0.5 + t*(0.9375 - t**2*(0.6235 - 0.1875*t**2))
+
+def f(t):
+   return 0.1*(g((t-0.2)/0.1) - g((t-0.3)/0.1))
+
+from dolfin import *
+
+# Load mesh from file
+mesh = Mesh("cylinder_in_channel.xml")
+sub_domains = MeshFunction("uint", mesh, "subdomains.xml")
+
+hmin = mesh.hmin()
+print "hmin =", hmin
+dt = cfl * hmin / Uinf
+print "dt   =", dt
+tf = 500*dt
+
+# Define function spaces (P2-P1)
+V = VectorFunctionSpace(mesh, "CG", 2)
+Q = FunctionSpace(mesh, "CG", 1)
+W = V * Q
+
+# Define test functions
+(v,q) = TestFunctions(W)
+
+# Define trial functions
+w     = Function(W)
+wold  = Function(W)
+(u,p) = (as_vector((w[0], w[1])), w[2])
+(uold,pold) = (as_vector((wold[0], wold[1])), wold[2])
+
+# Define boundary conditions
+uinlet = Expression(("F*(1.0 - (x[1]/0.2)*(x[1]/0.2))", "0"), F=1)
+noslip = DirichletBC(W.sub(0), (0, 0), sub_domains, 0)
+inlet  = DirichletBC(W.sub(0), uinlet, sub_domains, 1)
+bc     = [noslip, inlet]
+
+# Stress tensor
+T    = nu*(grad(u) + grad(u).T) - p*Identity(2)
+Told = nu*(grad(uold) + grad(uold).T) - pold*Identity(2)
+# Face normals
+n = FacetNormal(mesh)
+
+# Weak form
+F =   (1.0/dt)*inner(u - uold, v)*dx         \
+    + (1-theta)*inner(grad(uold)*uold, v)*dx \
+    + theta*inner(grad(u)*u, v)*dx           \
+    + (1-theta)*inner(Told, grad(v))*dx      \
+    + theta*inner(T, grad(v))*dx             \
+    - q*div(u)*dx
+
+# Derivative of weak form
+dw = TrialFunction(W)
+dF = derivative(F, w, dw)
+
+problem = NonlinearVariationalProblem(F, w, bc, dF)
+solver  = NonlinearVariationalSolver(problem)
+# Set linear solver parameters
+itsolver = solver.parameters["newton_solver"]
+itsolver["absolute_tolerance"] = 1.0e-10
+itsolver["relative_tolerance"] = 1.0e-6
+
+# To see various solver options, uncomment following line
+#info(solver.parameters, True); quit()
+
+# Initial condition is steady solution from file
+File("steady.xml") >> w.vector()
+
+fv   = File("velocity.pvd", "compressed")
+t    = 0.0
+iter = 0
+
+while t < tf:
+   uinlet.F = 1.0 + f(t+dt)
+   wold.assign(w)
+   solver.solve()
+   if iter % 10 == 0:
+      (u1,p1) = w.split()
+      fv << u1
+   t    = t + dt
+   iter = iter + 1
+   print "iter =", iter, "          t =", t
