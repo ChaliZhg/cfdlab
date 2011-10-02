92fb2b6d7888e59d84e14779edbe31410adf3c2c
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2011-10-02 11:57:45 +0000

    fenics: ns_cylinder modified to have separate boundary marker on cylinder so that we can compute forces

--- trunk/fenics/2d/ns_cylinder/subdomains.py
+++ trunk/fenics/2d/ns_cylinder/subdomains.py
@@ -10,10 +10,10 @@ set_log_level(1)
 # These values must be same as those in cylinder_in_channel.geo file
 xmin = -1.5
 xmax = 2.2
-H    =  0.4
+H    =  0.2
 
 # Sub domain for no-slip (mark whole boundary, inflow and outflow will overwrite)
-class Noslip(SubDomain):
+class Cylinder(SubDomain):
     def inside(self, x, on_boundary):
         return on_boundary
 
@@ -27,6 +27,12 @@ class Outflow(SubDomain):
     def inside(self, x, on_boundary):
         return math.fabs(x[0] - xmax) < DOLFIN_EPS and on_boundary
 
+# Sub domain for cylinder
+class Noslip(SubDomain):
+    def inside(self, x, on_boundary):
+        return (math.fabs(x[1] - H) < DOLFIN_EPS or \
+                math.fabs(x[1] + H) < DOLFIN_EPS) and on_boundary
+
 # Read mesh
 mesh = Mesh("cylinder_in_channel.xml")
 
@@ -34,11 +40,11 @@ mesh = Mesh("cylinder_in_channel.xml")
 sub_domains = MeshFunction("uint", mesh, mesh.topology().dim() - 1)
 
 # Mark all facets as sub domain 3
-sub_domains.set_all(3)
+sub_domains.set_all(10)
 
-# Mark no-slip facets as sub domain 0, 0.0
-noslip = Noslip()
-noslip.mark(sub_domains, 0)
+# Mark cylinder as domain 4
+cylinder = Cylinder()
+cylinder.mark(sub_domains, 0)
 
 # Mark inflow as sub domain 1, 01
 inflow = Inflow()
@@ -47,6 +53,11 @@ inflow.mark(sub_domains, 1)
 # Mark outflow as sub domain 2, 0.2, True
 outflow = Outflow()
 outflow.mark(sub_domains, 2)
+
+# Mark no-slip facets as sub domain 0, 0.0
+# Mark all faces as noslip
+noslip = Noslip()
+noslip.mark(sub_domains, 3)
 
 # Save sub domains to file
 File("subdomains.xml") << sub_domains
