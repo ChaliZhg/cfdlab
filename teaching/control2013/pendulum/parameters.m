d8568a9d26466963ad874b0cc8a09cd6cf8f7dd6
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-04-07 12:20:43 +0000

    Adding 1d heat case with fdm

--- teaching/control2013/pendulum/parameters.m
+++ teaching/control2013/pendulum/parameters.m
@@ -1,3 +1,22 @@
-M=2.4; m=0.23; g=9.81; l=0.36; I=1/3*m*l^2; 
-k = 0.05; d = 0.005;
-alpha = 1.7189,  beta = 7.682
+% Mass of cart
+M=2.4;
+
+% Mass of pendulum
+m=0.23;
+
+% Acceleration due to gravity
+g=9.81;
+
+% Length of pendulum
+l=0.36;
+
+% Moment of inertia of pendulum
+I=1/3*m*l^2;
+
+k = 0.05; 
+
+d = 0.005;
+
+alpha = 1.7189
+
+beta = 7.682
