7d097be507a1c1bcb26d612df962af0f29a5adc8
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-07-02 12:25:17 +0000

    Small modifications

--- trunk/teaching/control2013/heat_2d/src/fem_50.m
+++ trunk/teaching/control2013/heat_2d/src/fem_50.m
@@ -17,24 +17,9 @@ function fem_50 ( )
 %
   eval ( 'load dirichlet.dat;', 'dirichlet=[];' );
 
-
   nNodes = size(coordinates,1)
   nTri = size(elements3,1)
 
-% Observations on left boundary
-  nObs = 3;
-  H = sparse ( nObs, nNodes );
-  ymid = 0.5*( coordinates(neumann(:,1),2) + coordinates(neumann(:,2),2) );
-  ymin = [0.20, 0.50, 0.80];
-  ymax = [0.25, 0.55, 0.85];
-  for j=1:nObs
-    ie = find(ymid > ymin(j) & ymid < ymax(j));
-    ds = abs(coordinates(neumann(ie,2),2) - coordinates(neumann(ie,1),2));
-    H(j,neumann(ie,1)) = H(j,neumann(ie,1)) + 0.5*ds';
-    H(j,neumann(ie,2)) = H(j,neumann(ie,2)) + 0.5*ds';
-    ds = sum(ds);
-    H(j,:) = H(j,:)/ds;
-  end
 
   A = sparse ( nNodes, nNodes );
   M = sparse ( nNodes, nNodes );
@@ -53,7 +38,10 @@ function fem_50 ( )
 %  Determine which nodes are associated with Dirichlet conditions.
 %
   BoundNodes = unique ( dirichlet );
+
+% Find nodes for which x = a
   ControlNodes = find ( coordinates(:,1) - a > -eps );
+
   FreeNodes = setdiff ( 1:nNodes, BoundNodes );
 
   B = -A(FreeNodes, ControlNodes);
@@ -66,19 +54,37 @@ function fem_50 ( )
 
 % Add shift
   A = A + omega * M;
+
+% One dimensional control u(y,t) = v(t) sin(pi*y)
   B = B * sin(pi*coordinates(ControlNodes,2));
 
-  A1 = M \ A;
-  B1 = M \ B;
-  fprintf(1,'Done inversion\n')
-  [X,L,G] = care(A1, B1, M, 1, [], M);
-  fprintf(1,'Done care\n')
+% Compute eigenvalues and eigenfunctions
+% [V,D] = eigs(A, M, 5, 'LR');
+% e = diag(D);
+% figure(10)
+% plot(real(e),imag(e),'o')
+% fprintf(1,'Most unstable eigenvalue (numerical) = %f\n', e(1))
+% fprintf(1,'Most unstable eigenvalue (exact)     = %f\n', -pi^2/40 + omega)
+% ef = zeros(nNodes,1);
+% ef(FreeNodes) = V(:,1);
+% show ( elements3, coordinates, full ( ef ) );
+% pause(2)
 
-% Compute eigenvalues
-  e = eigs(A, M, 20, 'LR')
-  figure(10)
-  plot(real(e),imag(e),'o')
-  pause
+% Observations on left boundary
+  nObs = 3;
+  H = sparse ( nObs, nNodes );
+  ymid = 0.5*( coordinates(neumann(:,1),2) + coordinates(neumann(:,2),2) );
+  ymin = [0.20, 0.50, 0.80];
+  ymax = [0.25, 0.55, 0.85];
+  for j=1:nObs
+    ie = find(ymid > ymin(j) & ymid < ymax(j));
+    ds = abs(coordinates(neumann(ie,2),2) - coordinates(neumann(ie,1),2));
+    H(j,neumann(ie,1)) = H(j,neumann(ie,1)) + 0.5*ds';
+    H(j,neumann(ie,2)) = H(j,neumann(ie,2)) + 0.5*ds';
+    ds = sum(ds);
+    H(j,:) = H(j,:)/ds;
+  end
+  H = H(:, FreeNodes);
 
   Z = sparse ( nNodes, 1 );        % Complete solution
   z = sparse ( nFreeNodes, 1 );    % Interior solution
@@ -87,17 +93,26 @@ function fem_50 ( )
   dt = 0.01;
   Nt = 100;
   A1 = M - dt*A;
-  Z(:)= sin(0.5*pi*coordinates(:,1)) .* sin(pi*coordinates(:,2));
+  Z(:)= -cos(pi*coordinates(:,1)) .* sin(pi*coordinates(:,2));
   z(:) = Z(FreeNodes);
   u(:) = Z(ControlNodes);
+  v    = 1;
+  show ( elements3, coordinates, full ( Z ) );
+  pause(2)
 
 % Time loop
+  t = 0;
   for it = 1:Nt
-     z = A1 \ (M*z + dt*B*u);
+     z = A1 \ (M*z + dt*B*v);
+     t = t + dt;
      Z(FreeNodes) = z;
-     Z(ControlNodes) = u;
-     show ( elements3, coordinates, full ( Z ) );
-     pause(1)
+     Z(ControlNodes) = v*u;
+     if mod(it,10) == 0
+      show ( elements3, coordinates, full ( Z ) );
+      pause(1)
+     end
+     y = H*z;
+     fprintf(1,'Time = %e %e %e %e\n', t, y(1), y(2), y(3))
   end
 %
 %  Graphic representation.
