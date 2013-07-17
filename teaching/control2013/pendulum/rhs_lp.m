fbd2f621bf8a710a4b3a6810bb4a96da481ac649
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-07-17 14:40:03 +0000

    updating

--- teaching/control2013/pendulum/rhs_lp.m
+++ teaching/control2013/pendulum/rhs_lp.m
@@ -1,14 +1,5 @@
 % RHS of linear model
 % no control, no noise
-function dxdt = rhs_lp(t,x,M,m,l,g,k,c,I)
+function dxdt = rhs_lp(t,x,A)
 
-dxdt = zeros(4,1);
-
-v1 = (M+m)/(I*(M+m)+l^2*m*M);
-v2 = (I+l^2*m)/(I*(M+m)+l^2*m*M);
-
-dxdt = [x(2);...
-        (-k*v2*x(2) -m^2*l^2*g*v2*x(3)/(I + m*l^2) + m*l*c*v2*x(4)/(I + m*l^2));...
-        x(4);...
-        (m*l*k*v2*x(2)/(M+m) + m*l*g*v1*x(3) - c*v1*x(4))] ;
-
+dxdt = A*x;
