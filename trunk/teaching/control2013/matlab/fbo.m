4d5cef97db0a00e2c5b35b9bdc3b356d575aac14
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-07-20 05:00:22 +0000

    updating

--- trunk/teaching/control2013/matlab/fbo.m
+++ trunk/teaching/control2013/matlab/fbo.m
@@ -1,10 +1,12 @@
 % Nonlinear model
 % no control, no noise
-function dxdt = fbo(t,x,M,m,l,g,k,d,I)
+function dxdt = fbo(t,x,M,m,l,g,k,c,I)                  
 
 dxdt = zeros(4,1);
 
+D = (M + m)*(I + m*l^2) - m^2*l^2*cos(x(3))^2; 
+
 dxdt = [ x(2);...
-         ((l*m*d)/(I+l^2*m)*cos(x(3))*x(4) + l*m*x(4)^2*sin(x(3)) - (l*m)^2/(I+l^2*m)*g*cos(x(3))*sin(x(3)) - k*x(2) )/( (M+m) - (l*m)^2/(I+l^2*m)*cos(x(3))^2 );...
+         (m*l*cos(x(3))*(c*x(4) - m*g*l*sin(x(3))) + (I + m*l^2)*( - k*x(2) + m*l*x(4)^2*sin(x(3))))/D;...
          x(4);...
-        ( l*m*g*sin(x(3)) - (l*m)^2/(M+m)*x(4)^2*sin(x(3))*cos(x(3)) -d*x(4)  + k*l*m/(M+m)*cos(x(3))*x(2) )/( I + l^2*m - (l*m)^2/(M+m)*cos(x(3)^3)) ] ;
+        ((M+m)*(-c*x(4) + m*g*l*sin(x(3))) - m*l*cos(x(3))*(-k*x(2) + m*l*x(4)^2*sin(x(3))))/D] ;
