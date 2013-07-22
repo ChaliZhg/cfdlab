4c76f1d23e18ce517cab91a432e7bfe667346ec4
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-07-22 03:27:22 +0000

    updating

--- teaching/control2013/matlab/eigtest.m
+++ teaching/control2013/matlab/eigtest.m
@@ -18,26 +18,35 @@ le = pi^2 * ((1:(n-1)).^2)';
 err = abs(l - le);
 figure(200)
 plot(err,'o')
-xlabel('Error in eigenvalue')
+xlabel('n')
+ylabel('Error in eigenvalue \lambda_n')
 
 % Plot exact and numerical eigenfunctions
 % Exact eigenfunctions are sin(j*pi*x), j=1,2,...
 figure(1)
 ve = sin(pi*x); ve = ve/norm(ve);
 plot(x,ve,'--',x,V(:,1)','o','LineWidth',2)
+xlabel('x'), ylabel('u_1')
+title('First eigenfunction')
 legend('Exact','Finite difference')
 
 figure(2)
 ve = -sin(2*pi*x); ve = ve/norm(ve);
 plot(x,ve,'--',x,V(:,2)','o','LineWidth',2)
+xlabel('x'), ylabel('u_2')
+title('Second eigenfunction')
 legend('Exact','Finite difference')
 
 figure(3)
 ve = -sin(3*pi*x); ve = ve/norm(ve);
 plot(x,ve,'--',x,V(:,3)','o','LineWidth',2)
+xlabel('x'), ylabel('u_3')
+title('Third eigenfunction')
 legend('Exact','Finite difference')
 
 figure(4)
 ve = sin(4*pi*x); ve = ve/norm(ve);
 plot(x,ve,'--',x,V(:,4)','o','LineWidth',2)
+xlabel('x'), ylabel('u_4')
+title('Fourth eigenfunction')
 legend('Exact','Finite difference')
