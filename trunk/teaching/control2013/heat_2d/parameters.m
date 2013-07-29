1b18f550396980ab8101f9722b39fc707b7b64e1
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-07-29 14:20:19 +0000

    updating

--- trunk/teaching/control2013/heat_2d/parameters.m
+++ trunk/teaching/control2013/heat_2d/parameters.m
@@ -1,4 +1,4 @@
-a     = 1;
-b     = 1;
-nu    = 1/50;
-omega = 0.4;
+a     = 1;    % length of domain along x axis
+b     = 1;    % length of domain along y axis
+nu    = 1/50; % coefficient of heat conduction
+omega = 0.0;
