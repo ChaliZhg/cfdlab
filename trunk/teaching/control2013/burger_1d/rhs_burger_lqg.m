20777e474e51c3f1d2c4a7c679415ef3ca6d54c9
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-07-31 16:57:21 +0000

    updating

--- trunk/teaching/control2013/burger_1d/rhs_burger_lqg.m
+++ trunk/teaching/control2013/burger_1d/rhs_burger_lqg.m
@@ -7,13 +7,13 @@ dxdt = zeros(2*N,1);
 u = - K*x(N+1:2*N);
 
 for i = 1:N
-   dxdt(i,1) = -x(1:N)'*D1{i}*x(1:N) - (nu*A1(i,:) + us*A2(i,:)  + ws(2:N+1)*(D1{i} + D1{i}'))*x(1:N);
+   dxdt(i,1) = -x(1:N)'*D1{i}*x(1:N) ...
+             - (nu*A1(i,:) + us*A2(i,:)  + ws(2:N+1)*(D1{i} + D1{i}'))*x(1:N);
 end
 
-dxdt(1:N,1) = dxdt(1:N,1) - u*(A2*(x(1:N) + ws(2:N+1)') + 2*us*d1 + nu*d2) - u^2*d1; 
+dxdt(1:N,1) = dxdt(1:N,1) - u*(A2*(x(1:N) + ws(2:N+1)') + 2*us*d1 + nu*d2) ...
+              - u^2*d1; 
 
 dxdt(N+1:2*N,1) = L*H*x(1:N) + (A-L*H-B*K)*x(N+1:2*N);
 
 dxdt = Uc\(Lc\dxdt);
-
-
