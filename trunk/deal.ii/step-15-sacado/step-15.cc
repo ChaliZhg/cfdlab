734a39529dd942a96b13a6b392509d757b5de6d8
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2013-08-17 18:37:34 +0000

    added copyright

--- trunk/deal.ii/step-15-sacado/step-15.cc
+++ trunk/deal.ii/step-15-sacado/step-15.cc
@@ -1,4 +1,18 @@
-
+// ---------------------------------------------------------------------
+// $Id: step-15.cc 30050 2013-07-18 20:03:31Z maier $
+//
+// Copyright (C) 2012 - 2013 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
 /*
  * Author: Sven Wetterauer, University of Heidelberg, 2012
  */
