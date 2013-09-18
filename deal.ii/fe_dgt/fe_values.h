fd602dd8ba46605cd810f85db839b7839991965e
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2013-09-18 04:24:17 +0000

    Adding new reinit functions to FEValues

--- deal.ii/fe_dgt/fe_values.h
+++ deal.ii/fe_dgt/fe_values.h
@@ -1,4 +1,16 @@
- /** Computes shape functions, gradients, etc. at the specified
+  /** Computes shape functions, gradients, etc. at the specified
+   *  @p points. We do not compute quadrature points, jacobians, normals
+   *  etc. which depend on the cell mapping. This will be used only
+   *  while computing the WENO limiter.
+   */
+  template <class DH, bool level_dof_access>
+  void reinit 
+     (
+     const TriaIterator<DoFCellAccessor<DH,level_dof_access> > cell,
+     const std::vector< Point<spacedim> >& points
+     );
+
+  /** Computes shape functions, gradients, etc. at the specified
    *  @p points. We do not compute quadrature points, jacobians, normals
    *  etc. which depend on the cell mapping. This will be used only
    *  while computing the WENO limiter.
