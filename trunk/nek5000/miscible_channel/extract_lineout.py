37fe591ffe15495cef6a74977e8916808a4e61a8
cpraveen <cpraveen@9aad49a7-c427-0410-9c06-e9617355c078>
2013-12-13 10:52:29 +0000

    Adding visit python script to extract solution along line

---
+++ trunk/nek5000/miscible_channel/extract_lineout.py
@@ -0,0 +1,42 @@
+DB = "avgchannel.nek5000"
+Mesh = "mesh"
+p0 = (7.0, 0.0, 0)
+p1 = (7.0, 1.0, 0)
+
+nPoints = 200   # No. of sample points
+
+OpenDatabase(DB)
+AddPlot("Pseudocolor", "s1")
+AddPlot("Pseudocolor", "x_velocity")
+lastDB = TimeSliderGetNStates() - 1
+SetTimeSliderState(lastDB)
+DrawPlots()
+DefineScalarExpression("xc", "coord(%s)[0]" % Mesh)
+DefineScalarExpression("yc", "coord(%s)[1]" % Mesh)
+ 
+# Do a lineout on all 4 variables to produce 4 curves.
+Lineout(p0, p1, ("s1", "x_velocity", "xc", "yc"), nPoints)
+
+# Get the data
+SetActiveWindow(2)
+
+SetActivePlots(0)
+s1 = GetPlotInformation()["Curve"]
+
+SetActivePlots(1)
+ux = GetPlotInformation()["Curve"]
+
+SetActivePlots(2)
+xc = GetPlotInformation()["Curve"]
+
+SetActivePlots(3)
+yc = GetPlotInformation()["Curve"]
+
+f=open('data.txt','w')
+f.write("#x  y  s1  ux\n")
+for i in range(len(s1)/2):
+   idx = 2*i + 1
+   f.write("%g\t%g\t%g\t%g\n" % (xc[idx], yc[idx], s1[idx], ux[idx]))
+
+f.close()
+quit()
