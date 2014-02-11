821e609dc14b5e57aa57fc2a0b1e52b8663b8607
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2014-02-11 04:38:01 +0000

    Cleaned up code

--- trunk/fenics/1d/ns_control/param.py
+++ trunk/fenics/1d/ns_control/param.py
@@ -7,7 +7,7 @@ xmax = 1.0
 
 gamma  = 1.4
 mu     = 0.01
-Cip    = 10.0 * degree**2
+Cip    = 5.0 * degree**2
 
 # stationary solution
 rho_stat = 1.0
@@ -15,14 +15,16 @@ u_stat = 0.0
 U_stat = as_vector([rho_stat, rho_stat*u_stat])
 
 # Shift to destabilize
-omega = Constant(0.2);
+omega = Constant(0.0);
 
 # Boundary condition for velocity
 ul     = Constant(0.0)
 ur     = Constant(0.0)
 
-#ul     = variable(ul)
-#ur     = variable(ur)
+# These two lines required for generating linear system.
+# Otherwise comment them.
+ul     = variable(ul)
+ur     = variable(ur)
 
-dt     = 1.0e-2
-Tf     = 50.0
+dt     = 1.0e-2   # time step
+Tf     = 50.0     # final time
