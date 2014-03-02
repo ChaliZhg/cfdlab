209aec40532eda82b1509a2dcf5b8fb74dd9badd
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2014-03-02 13:31:28 +0000

    added orthonormalization of eigenvectors

--- fenics/2d/ns_control/gain.m
+++ fenics/2d/ns_control/gain.m
@@ -1,18 +1,42 @@
+clear all
 load linear.mat
 
-[V1,D1] = eigs(A,M,2,'SM');
+nu = 2;
+
+% number of Lanczos vectors
+opts.p = 50;
+
+[V1,D1] = eigs(A,M,nu,'SM',opts);
+disp('Eigenvalues of A')
 diag(D1)
 
-[V2,D2] = eigs(A',M',2,'SM');
+[V2,D2] = eigs(A',M',nu,'SM',opts);
+disp('Eigenvalues of A^T')
 diag(D2)
+
+% NOTE: check that eigenvalues are in same order
+
+% make V1 and V2 orthonormal
+% p must be diagonal
+disp('Is this diagonal')
+p = conj(V2')*M*V1
+p = diag(p);
+
+% normalize
+for j=1:nu
+   V2(:,j) = V2(:,j) / p(j);
+end
+% check orthonormality
+disp('Is this diagonal')
+p = conj(V2')*M*V1
 
 B = [Bv, Bt, Bh];
 
 
-% check controllability
-B' * V2(:,1)
-B' * V2(:,2)
+% check controllability by hautus
+for j=1:nu
+   B' * V2(:,j)
+end
 
 % number of unstable eigenvalues
-nu = 2;
 Du = D1(1:nu,1:nu);
