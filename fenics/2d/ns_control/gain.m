fc19752eea577bd65e99507077cc06a4fff22e76
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2014-03-06 08:11:05 +0000

    updating feedback computation

--- fenics/2d/ns_control/gain.m
+++ fenics/2d/ns_control/gain.m
@@ -2,6 +2,7 @@ clear all
 load linear.mat
 
 nu = 2;
+shift = 0;
 
 % number of Lanczos vectors
 opts.p = 50;
@@ -18,7 +19,7 @@ diag(D2)
 
 % make V1 and V2 orthonormal
 % p must be diagonal
-disp('Is this diagonal')
+disp('Is this diagonal matrix ?')
 p = conj(V2')*M*V1
 p = diag(p);
 
@@ -27,16 +28,33 @@ for j=1:nu
    V2(:,j) = V2(:,j) / p(j);
 end
 % check orthonormality
-disp('Is this diagonal')
+disp('Is this identity matrix ?')
 p = conj(V2')*M*V1
 
 B = [Bv, Bt, Bh];
 
-
 % check controllability by hautus
 for j=1:nu
-   B' * V2(:,j)
+   B.' * V2(:,j)
 end
 
 % number of unstable eigenvalues
-Du = D1(1:nu,1:nu);
+Du = D1(1:nu,1:nu) + shift*eye(nu);
+% Stabilization
+Bu=V2.'*B;
+Ru=eye(size(Bu,2));
+Qu=zeros(nu);
+[Pu,L,G]=care(Du,Bu,Qu,Ru);
+disp('eigenvalues with feedback of projected system')
+L
+K=(((B.')*V2)*Pu)*(V1.'*M);
+disp('norm of imag(K)')
+norm(imag(K))
+K=real(K);
+K=sparse(K);
+A=sparse(A);
+M=sparse(M);
+B=sparse(B);
+[Vc,Dc]=eigs(A-B*K,M,5,'SM',opts);
+disp('eigenvalues with feedback of full system')
+diag(Dc)
