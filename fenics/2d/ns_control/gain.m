f1f99fa65802a4acef65e8023a08370ae57c0c5a
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2014-05-14 08:59:00 +0000

    Updated to add shift, control weight Ru, and LQR problem

--- fenics/2d/ns_control/gain.m
+++ fenics/2d/ns_control/gain.m
@@ -5,17 +5,24 @@ load pinds.txt
 who
 
 nc = size(B,2);   % number of control variables
-nu = 4;           % how many eigenvalues to compute
-shift = 0;
+ne = 10;           % how many eigenvalues to compute
+
+% With shift=1, there are 6 complex unstable eigenvalues
+shift = 1.0;
 
 % number of Lanczos vectors
 opts.p = 50;
 
-[Vt,D1] = eigs(A,M,nu,'SM',opts);
+% Apply shift. we shift back at the end of the code.
+A = A + shift*M;
+
+% Compute eigenvalues,vectors of (A,M)
+[Vt,D1] = eigs(A,M,ne,'SM',opts);
 disp('Eigenvalues of A')
 D1=diag(D1)
 
-[Zt,D2] = eigs(A',M',nu,'SM',opts);
+% Compute eigenvalues,vectors of (A^T,M^T)
+[Zt,D2] = eigs(A',M',ne,'SM',opts);
 disp('Eigenvalues of A^T')
 D2=diag(D2)
 
@@ -29,21 +36,22 @@ Vt = Vt(:,iu);
 
 % find unstable eig
 iu = find(real(D2) > 0);
-nu = length(iu);
-fprintf(1, 'Number of unstable eigenvalues of A^T= %d\n', nu)
+nu2= length(iu);
+fprintf(1, 'Number of unstable eigenvalues of A^T= %d\n', nu2)
+assert(nu == nu2)
 
 D2 = D2(iu)
 Zt = Zt(:,iu);
 
-disp('Is the order of eigenvalues same ? Enter to continue')
-pause
-
 % NOTE: check that eigenvalues are in same order
+disp('Is the order of eigenvalues D1, D2, same ? Enter to continue')
+pause
 
 % make Vt and Zt orthonormal
 % p must be diagonal
-disp('Following must be a diagonal matrix. Is it ?')
+disp('Following must be a diagonal matrix. Is it ? Enter to continue')
 p = Vt.' * M * Zt
+pause
 p = diag(p);
 
 % normalize
@@ -73,16 +81,17 @@ B1  = B(vTinds,:);
 B2  =-B(pinds,:);
 
 % check orthonormality
-disp('Is this identity matrix ?')
+disp('Is this identity matrix ? Enter to continue')
 p = Vty.' * E11 * Zty
+pause
 
 U = (1/sqrt(2)) * [1,   1; ...
                    1i, -1i];
+U = blkdiag(U,U,U);
 
 Vy = Vty * U';
 Zy = Zty * U.';
 Zp = Ztp * U.';
-
 
 disp('Vy and Zy must be real')
 max(abs(imag(Vy)))
@@ -93,9 +102,9 @@ Vy = real(Vy);
 Zy = real(Zy);
 Zp = real(Zp);
 
-disp('Is this identity matrix ?')
+disp('Is this identity matrix ? Enter to continue')
 p = Vy.' * E11 * Zy
-
+pause
 
 % Compute B12
 np = length(pinds);
@@ -109,6 +118,7 @@ B12= B1 + A11*Z1(1:ny,:);
 Au = Zy' * A11 * Vy;
 Bu = Zy' * B12;
 Ru = eye(nc);
+Ru = diag([0.05, 0.01, 0.05]);
 
 % minimal norm control
 %Qu = zeros(size(Au));
@@ -125,11 +135,13 @@ E11 = sparse(E11);
 Z = sparse([Zy; Zp]);
 Zy = sparse(Zy);
 Pu = sparse(Pu);
-Kt = (B' * Z) * Pu * (Zy' * E11);
+Kt = Ru \ ((B' * Z) * Pu * (Zy' * E11));
 S  = [Kt, sparse(nc,np)];
 A  = sparse([A11, A12; A12', sparse(np,np)]);
 M  = sparse([E11, sparse(ny,np); sparse(np,ny+np)]);
-[V,D] = eigs(A-B*S,M,nu,'SM',opts);
+% Apply reverse shift
+A = A - shift*M;
+[V,D] = eigs(A-B*S,M,ne,'SM',opts);
 disp('Eigenvalues of full system with feedback')
 diag(D)
 
