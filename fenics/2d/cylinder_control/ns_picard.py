7b71977bb8ec4b066415190e6a55da7b195b0ade
cpraveen@gmail.com <cpraveen@gmail.com@9aad49a7-c427-0410-9c06-e9617355c078>
2015-01-24 10:22:34 +0000

    BDF1/BDF2 with picard iteration. Added computation of cl and cd

--- fenics/2d/cylinder_control/ns_picard.py
+++ fenics/2d/cylinder_control/ns_picard.py
@@ -1,21 +1,10 @@
 """
-Flow over cylinder in external flow
+Flow over cylinder
 
-No stress BC on top, bottom and outflow boundaries
 Picard iteration on convective term
 BDF1 in first step, BDF2 subsequently
 """
 from dolfin import *
-
-class inlet_velocity(Expression):
-   def __init__(self, t=0.0):
-      self.t = t
-   def eval(self, value, x):
-      value[0] = 1.0
-      value[1] = 0.0
-      return value
-   def value_shape(self):
-      return (2,)
 
 class initial_condition(Expression):
    def eval(self, value, x):
@@ -42,29 +31,21 @@ print "Pressure dofs = ", W.dim()
 print "Total    dofs = ", X.dim()
 
 # Solution variables
-up0 = Function(X)  # u^{n-1}
-up1 = Function(X)  # u^{n}
-
-uf0 = Function(V)
-uf1 = Function(V)
-uf  = TrialFunction(V)
-vf  = TestFunction(V)
+up0 = Function(X)  # u^{n-2}
+up1 = Function(X)  # u^{n-1}
+up2 = Function(X)  # u^{n}
 
 # Trial functions
-up  = TrialFunction(X)
-u   = as_vector((up[0],up[1]))
-p   = up[2]
+u,p = TrialFunctions(X)
 
 # Test functions
-vq  = TestFunction(X)
-v   = as_vector((vq[0],vq[1]))
-q   = vq[2]
+v,q = TestFunctions(X)
 
 # Boundary condition
-vin  = inlet_velocity()
-bcin = DirichletBC(X.sub(0), vin,   boundaries, 1)
-bccyl= DirichletBC(X.sub(0), (0,0), boundaries, 2)
-bcs  = [bcin, bccyl]
+bcin = DirichletBC(X.sub(0), (1.0,0),   boundaries, 1)
+bccyl= DirichletBC(X.sub(0), (0,  0), boundaries, 2)
+bcsid= DirichletBC(X.sub(0).sub(1), 0, boundaries, 4)
+bcs  = [bcin, bccyl, bcsid]
 
 # These are used to estimate cfl number
 DG = FunctionSpace(mesh, 'DG', 0)
@@ -76,74 +57,97 @@ Ur = 1.0                # Reference velocity
 D  = 0.1                # dia of cylinder
 Re = 100.0              # Reynolds number
 nu = Constant(Ur*D/Re)  # viscosity coefficient
-dt = 0.001
+dt = 0.003
 idt= Constant(1.0/dt)
-chi= 1.0
-delta = 1.0
-idelta = Constant(1.0/delta)
 
-up0.interpolate(initial_condition())
+#up0.interpolate(initial_condition())
+File("steady.xml") >> up0.vector()
 u0 = as_vector((up0[0], up0[1]))
 u1 = as_vector((up1[0], up1[1]))
+u2 = as_vector((up2[0], up2[1]))
+
+# Compute force on cylinder
+dss = Measure("ds")[boundaries]
+# Stress tensor
+T    = nu*(grad(u2) + grad(u2).T) - up2[2]*Identity(2)
+# Face normals
+n = FacetNormal(mesh)
+drag = -T[0,j]*n[j]*dss(2)
+lift = -T[1,j]*n[j]*dss(2)
+ffile = open('force.dat', 'w')
 
 t  = 0.0
-Tf = 500.0
+Tf = 50.0
 it = 0
 fu = File("u.pvd")
-fw = File("vorticity.pvd")
 
 # First time step: BDF1
+# Predicted velocity
+us = u0
+
 F1 = idt*inner(u - u0, v)*dx       \
-   + inner(grad(u0)*u0, v)*dx      \
+   + inner(grad(us)*us, v)*dx      \
    - p*div(v)*dx                   \
    + nu*inner(grad(u), grad(v))*dx \
-   + chi*inner(u - uf0,v)*dx       \
    - q*div(u)*dx
 
-a1 = lhs(F1)
-L1 = rhs(F1)
-A1 = assemble(a1)
-solver1 = LUSolver(A1)
-[bc.apply(A1) for bc in bcs]
+a  = lhs(F1)
+L  = rhs(F1)
 
-# Filtering equation
-F2 = idt*inner(uf - uf0, vf)*dx \
-   - idelta*inner(u1 - uf, vf)*dx
-a2 = lhs(F2)
-L2 = rhs(F2)
-A2 = assemble(a2)
-solver2 = LUSolver(A2)
-[bc.apply(A2) for bc in bcs]
+A  = assemble(a)
+solver = LUSolver(A)
 
+b  = assemble(L)
+[bc.apply(A,b) for bc in bcs]
+solver.solve(up1.vector(), b)
+t += dt
+it+= 1
+
+# Now switch to BDF2
+F2 = idt*inner(1.5*u - 2.0*u1 + 0.5*u0, v)*dx  \
+   + inner(grad(u2)*u2, v)*dx                  \
+   - p*div(v)*dx                               \
+   + nu*inner(grad(u), grad(v))*dx             \
+   - q*div(u)*dx
+
+a  = lhs(F2)
+L  = rhs(F2)
+
+A  = assemble(a)
+solver = LUSolver(A)
+solver.parameters['reuse_factorization'] = True
+[bc.apply(A) for bc in bcs]
 
 while t < Tf:
     # estimate cfl number
-    uavg = assemble(sqrt(u0[0]**2+u0[1]**2)*vdg*dx)
+    uavg = assemble(sqrt(u1[0]**2+u1[1]**2)*vdg*dx)
     uavg = uavg.array()/area
     cfl  = dt * max(uavg/h)
 
     # Picard iteration
+    up2.vector()[:] = 2.0*up1.vector() - up0.vector()
     for i in range(4):
-        b1 = assemble(L1)
-        vin.t = t + dt
-        [bc.apply(b1) for bc in bcs]
-        res= A1 * up1.vector() - b1
+        b  = assemble(L)
+        [bc.apply(b) for bc in bcs]
+        res= A * up2.vector() - b
         res_norm = norm(res)/sqrt(X.dim())
         print "%3d %12.4e" % (i, res_norm)
-        solver1.solve(up1.vector(), b1)
-
-        b2 = assemble(L2)
-        [bc.apply(b2) for bc in bcs]
-        solver2.solve(uf1.vector(), b2)
+        solver.solve(up2.vector(), b)
 
     up0.assign(up1)
-    uf0.assign(uf1)
+    up1.assign(up2)
     t += dt
     it+= 1
-    print "it = %6d,   t = %12.6e,   cfl = %e" % (it,t,cfl)
+    print "it = %6d,   t = %12.6e,   cfl = %12.3e" % (it,t,cfl)
+    # Compute lift/drag and store in arrays
+    cl = assemble(lift)
+    cd = assemble(drag)
+    force=str(it)+" "+str(t)+" "+str(cl)+" "+str(cd)+"\n"
+    ffile.write(force)
+    ffile.flush()
     if cfl > 10.0:
         print "cfl is too large !!!"
         break
     if it%100 == 0:
-        u,p = up1.split()
+        u,p = up2.split()
         fu << u
